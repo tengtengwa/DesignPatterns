f8451eddbb8bbfdc0c043743b6d8fcf7056a108b
tengtengwa <1511900943@qq.com>
2022-10-06 16:43:13 +0800

    策略模式

---
+++ src/behaviorPatter/strategy/Strategy.kt
@@ -0,0 +1,48 @@
+package behaviorPatter.strategy
+
+fun main() {
+    val calculator = PriceCalculator().apply {
+        strategy = TaxiStrategy()
+    }
+    println(calculator.calculatePrice(10))
+
+    calculator.strategy = BusStrategy()
+    println(calculator.calculatePrice(10))
+}
+
+/**
+ * 策略模式
+ * 行为型设计模式，一个类的行为或算法可以在运行时更改。
+ * Context类持有一个策略类对象，其中通过多态调用具体策略类的方法来实现不同的行为。
+ *
+ * 优点：符合开放封闭原则，扩展性良好
+ * 缺点：增加新的策略类时需要新增类
+ */
+
+class PriceCalculator {
+    lateinit var strategy: Strategy
+
+    fun calculatePrice(km: Int): Int {
+        return strategy.calculatePrice(km)
+    }
+}
+
+interface Strategy {
+    fun calculatePrice(km: Int): Int
+}
+
+class BusStrategy : Strategy {
+    override fun calculatePrice(km: Int): Int {
+        return if (km <= 5) {
+            10
+        } else if (km <= 10) {
+            12
+        } else {
+            15
+        }
+    }
+}
+
+class TaxiStrategy : Strategy {
+    override fun calculatePrice(km: Int) = km * 2
+}
